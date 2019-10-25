c015036beefb1745eba2b05c722df4af4734331f
Matthias Sturm <msturm@msturm.com>
2019-10-25 13:32:52 +0200

    added script and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# rss2instagram
+
+## What this script does
+- get featured image from latest post in RSS feed
+- put layer.png above it
+- post it on your Instagram story
+
+## What it doesn't
+- cooking a meal
---
+++ rss2instagram.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import feedparser
+import re
+import requests
+from io import BytesIO
+from PIL import Image
+from resizeimage import resizeimage
+
+sys.path.append(os.path.join(sys.path[0], "../"))
+from instabot import Bot # noqa: E402
+
+def get_last_post(feed_url):
+	feed_content = feedparser.parse(feed_url)
+	post = []
+	post.append(feed_content['items'][0]['title'])
+	post.append(feed_content['items'][0]['link'])
+	post.append(feed_content['items'][0]['published'])
+	post.append(re.search('(?P<url>https?://[^\s"]+)', feed_content['items'][0]['summary_detail']['value']).group('url'))
+	return post
+
+def get_story_image(feed_url):
+	post = get_last_post(feed_url)
+	response = requests.get(post[3])
+	cover = Image.open(BytesIO(response.content))
+
+	w, h = cover.size
+	new_w, new_h = 1080, 1920
+
+	cover = resizeimage.resize_cover(cover, [new_w, new_h])
+
+	# add foreground layer from rgba png file
+	layer = Image.open('layer.png').convert('RGBA')
+	cover.paste(layer, (0, 0), layer)
+
+	return cover
+
+def post_to_story(img):
+	bot = Bot()
+	bot.login()
+	bot.upload_story_photo(img)
+
+url = 'http://blog.local/feed/'
+cover = get_story_image(url)
+cover.save('cover.jpg')
+post_to_story('cover.jpg')
