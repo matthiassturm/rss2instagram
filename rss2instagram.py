fa78b3b46e64917db775ef78f6b48fe5e335b8af
Matthias Sturm <msturm@msturm.com>
2019-11-02 16:27:20 +0100

    added index to post getter

--- rss2instagram.py
+++ rss2instagram.py
@@ -10,17 +10,17 @@ from resizeimage import resizeimage
 sys.path.append(os.path.join(sys.path[0], "../"))
 from instabot import Bot # noqa: E402
 
-def get_last_post(feed_url):
+def get_post(feed_url, index):
 	feed_content = feedparser.parse(feed_url)
 	post = []
-	post.append(feed_content['items'][0]['title'])
-	post.append(feed_content['items'][0]['link'])
-	post.append(feed_content['items'][0]['published'])
-	post.append(re.search('(?P<url>https?://[^\s"]+)', feed_content['items'][0]['summary_detail']['value']).group('url'))
+	post.append(feed_content['items'][index]['title'])
+	post.append(feed_content['items'][index]['link'])
+	post.append(feed_content['items'][index]['published'])
+	post.append(re.search('(?P<url>https?://[^\s"]+)', feed_content['items'][index]['summary_detail']['value']).group('url'))
 	return post
 
 def get_story_image(feed_url):
-	post = get_last_post(feed_url)
+	post = get_post(feed_url, 0)
 	response = requests.get(post[3])
 	cover = Image.open(BytesIO(response.content))
 
@@ -40,7 +40,7 @@ def post_to_story(img):
 	bot.login()
 	bot.upload_story_photo(img)
 
-url = 'http://blog.local/feed/'
+url = 'http://berlingraffiti.de/feed/'
 cover = get_story_image(url)
 cover.save('cover.jpg')
 post_to_story('cover.jpg')
